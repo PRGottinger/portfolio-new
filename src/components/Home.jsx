8312f5fe4d09eaac75c60cb84fec508d89492654
Paul Gottinger <pgottinger@outlook.com>
2022-06-14 01:20:20 -0500

    about/skills

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+{/* Container*/}
+<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+    <p className='text-[#b60b0b]'>Hi, my name is</p>
+    <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Paul Gottinger</h1>
+    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a full stack developer</h2>
+    <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a developer with a passion for crafting compelling user stories and transforming them into exceptionally-designed digital experiences. I specialize in working with the MERN stack. I also possess strong communication and project managment skills, which I developed over the seven years I worked as a journalist and editor in digital media.</p>
+    <div>
+        <button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#b60b0b] hover:border-[#b60b0b]'>View Work 
+        <span className='group-hover:rotate-90 duration-300'>
+        <HiArrowNarrowRight className='ml-3' />
+        </span>
+        </button>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
@@ -8,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-<img src={logo} alt='Logo Image' style={{width: '50px'}}/>
+<img src={logo} alt='Logo' style={{width: '50px'}}/>
         </div>
 
         {/* menu */}
@@ -40,7 +42,34 @@ const Navbar = () => {
         </ul>
 
         {/* Social Icons */}
-        <div className='hidden'></div>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        LinkedIn <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Email <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' 
+                    href='/'>
+                        Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
     </div>
   )
