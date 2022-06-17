e6b0edd72025bf8415a693b01821885c95aa76c8
Paul Gottinger <pgottinger@outlook.com>
2022-06-16 23:18:13 -0500

    work spacing

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,7 +15,6 @@ const Work = () => {
 
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
@@ -65,8 +64,8 @@ const Work = () => {
               </div>
             </div>
           </div>
-           {/* Grid Item */}
-           <div
+          {/* Grid Item */}
+          <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
           >
@@ -93,7 +92,7 @@ const Work = () => {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
+            className="shadow-lg shadow-[#041216] group container rounded-md flex justify-center items-centermx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
@@ -114,17 +113,17 @@ const Work = () => {
               </div>
             </div>
           </div>
-           {/* Grid Item */}
-           <div
+          {/* Grid Item */}
+          <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
+            <div className="opacity-0 pt-12 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 React JS Application
               </span>
-              <div className="pt-8 text-center">
+              <div className="pt-4 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
@@ -145,14 +144,14 @@ const Work = () => {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div className="opacity-0 pt-14 mt-2 group-hover:opacity-100">
+              <span className="text-2xl pt-10 pl-3 font-bold text-white tracking-wider">
                 React JS Application
               </span>
-              <div className="pt-8 text-center">
+              <div className="pt-4 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
+                    App
                   </button>
                 </a>
                 <a href="/">
@@ -162,13 +161,12 @@ const Work = () => {
                 </a>
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Video Walkthrough
+                    Video
                   </button>
                 </a>
               </div>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
