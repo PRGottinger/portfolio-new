241c7256e4d2543c96451e1edce25b2996057f0f
Paul Gottinger <pgottinger@outlook.com>
2022-06-20 01:25:52 -0500

    photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Paul from "../assets/paul.png";
+import Paul from "../assets/paul-photo6.png";
 
 
 const About = () => {
@@ -8,7 +8,7 @@ const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-[#b60b0b]">
+            <p className="text-4xl font-bold inline border-b-4 justify-center border-[#b60b0b]">
               About
             </p>
           </div>
