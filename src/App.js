ef2136805e4b00497788dfb73dfcf3d3e914f633
Paul Gottinger <pgottinger@outlook.com>
2022-06-20 20:56:30 -0500

    video button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import Navbar from './components/Navbar';
 import Skills from "./components/Skills";
 import Work from './components/Work';
-import Footer from './components/Footer'
+
 
 
 
@@ -17,7 +17,7 @@ function App() {
       <Skills />
       <Work />
       <Contact/>
-      {/* <Footer/> */}
+     
     </div>
   );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import WorkImg from "../assets/workImg.jpeg";
 import realEstate from "../assets/realestate.jpg";
+import Home from "../assets/home.png"
+import Festival from "../assets/fes.png"
+import Travel from "../assets/travel.png"
+import Blog from "../assets/blog.png"
+import Budget from "../assets/budget.png"
+import Run from "../assets/run.png"
+
 
 const Work = () => {
   return (
@@ -17,144 +24,21 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${Home})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 pt-14 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+              <span className="text-2xl pl-20 items-center font-bold text-white tracking-wider">
+                Dev Drip
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 pt-14 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 pt-14 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#041216] group container rounded-md flex justify-center items-centermx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 pt-14 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 pt-14 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-4 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 pt-14 mt-2 group-hover:opacity-100">
-              <span className="text-2xl pt-10 pl-3 font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-4 text-center">
-                <a href="/">
+                <a href="https://calm-bayou-49842.herokuapp.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     App
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/PRGottinger/dev-drip">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -164,6 +48,140 @@ const Work = () => {
                     Video
                   </button>
                 </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Festival})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 pt-14 group-hover:opacity-100">
+              <span className="text-2xl pl-4 font-bold text-white tracking-wider">
+                Festival of Nations
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://powerful-mesa-61465.herokuapp.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    App
+                  </button>
+                </a>
+                <a href="https://github.com/PRGottinger/festivalOfNations">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Video
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Travel})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 pt-14 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Travel Briefing
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://pjlawler.github.io/destination-briefing/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    App
+                  </button>
+                </a>
+                <a href="https://github.com/PRGottinger/destination-briefing">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Blog})` }}
+            className="shadow-lg shadow-[#041216] group container rounded-md flex justify-center items-centermx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 pt-14 group-hover:opacity-100">
+              <span className="text-2xl pl-16 font-bold text-white tracking-wider">
+                Tech Blog
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://stark-caverns-96055.herokuapp.com">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    App
+                  </button>
+                </a>
+                <a href="https://github.com/PRGottinger/community-posting-board">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Video
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Budget})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 pt-14 group-hover:opacity-100">
+              <span className="text-2xl pl-4 font-bold text-white tracking-wider">
+                Budget App
+              </span>
+              <div className="pt-4 text-center">
+                <a href="https://afternoon-reef-93872.herokuapp.com">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    App
+                  </button>
+                </a>
+                <a href="https://github.com/PRGottinger/budget-tracker">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Run})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-centermx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 pt-14 mt-2 group-hover:opacity-100">
+              <span className="text-2xl pt-10 pl-3 font-bold text-white tracking-wider">
+                Run Buddy
+              </span>
+              <div className="pt-4 text-center">
+                <a href="https://prgottinger.github.io/run-buddy-page/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    App
+                  </button>
+                </a>
+                <a href="https://github.com/PRGottinger/run-buddy-page">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                
               </div>
             </div>
           </div>
